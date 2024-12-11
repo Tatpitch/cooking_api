517de48d23e973bf9b71d7c25c910ff5ed65052a
pta <pta.new26@gmail.com>
2024-12-11 21:59:36 +0300

    add settings and router to main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 import uvicorn
+from api import router as api_router
+from core.config import settings
 
 # создание прил
 main_app = FastAPI()
+# все роуты
+main_app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
 
 
 if __name__ == "__main__":
     # настройка подключения к серверу
     uvicorn.run(
         "main:main_app",
-                reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
     )
