ac0e8a6b11ddd836a48273bbd584908515bbc592
pta <pta.new26@gmail.com>
2024-12-11 21:26:07 +0300

    create fastpi app

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-def sum_two(a: int, b: int) -> int:
-    return a + b
+from fastapi import FastAPI
+import uvicorn
+
+# создание прил
+main_app = FastAPI()
+
+
+if __name__ == "__main__":
+    # настройка подключения к серверу
+    uvicorn.run(
+        "main:main_app",
+                reload=True,
+    )
--- test/test_sum_two.py
+++
@@ -1,5 +0,0 @@
-from src.main import sum_two
-
-def test_sum_two():
-    result = sum_two(2, 4)
-    assert result == 6
