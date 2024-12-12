6a555c1276bf690ee330d8b424f6ecff928fe129
pta <pta.new26@gmail.com>
2024-12-12 21:48:01 +0300

    add lifespan to main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+from contextlib import asynccontextmanager
 from api import router as api_router
 from core.config import settings
 
 # создание прил
 main_app = FastAPI()
+
+
+# Отдаются и закрываются все сессии для работы с БД
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 # все роуты
 main_app.include_router(
     api_router,
